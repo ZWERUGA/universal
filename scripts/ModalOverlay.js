45008e107d040ca4a581a9d71b4ce1a459bfff5c
Marat <maratnasyrov97@gmail.com>
2025-02-06 19:19:40 +0300

    Add mobile menu

---
+++ scripts/ModalOverlay.js
@@ -0,0 +1,54 @@
+class ModalOverlay {
+  selectors = {
+    burgerButton: "[data-js-burger-button]",
+    modalOverlay: "[data-js-modal-overlay-menu]",
+    modalOverlayInner: "[data-js-modal-overlay-inner]",
+    modalCloseButton: "[data-js-modal-overlay-close]",
+    modalLink: ".modal-overlay__link",
+  };
+
+  stateClasses = {
+    modalOverlayIsActive: "modal-overlay--is-active",
+    modalOverlayInnerIsActive: "modal-overlay__inner--is-active",
+    modalOpen: "modal-open",
+  };
+
+  constructor() {
+    this.burgerButtonElement = document.querySelector(this.selectors.burgerButton);
+    this.modalOverlayElement = document.querySelector(this.selectors.modalOverlay);
+    this.modalOverlayInnerElement = document.querySelector(this.selectors.modalOverlayInner);
+    this.modalCloseButtonElement = this.modalOverlayInnerElement.querySelector(
+      this.selectors.modalCloseButton
+    );
+    this.modalLinks = this.modalOverlayElement.querySelectorAll(this.selectors.modalLink);
+    this.bodyElement = document.querySelector("body");
+    this.bindEvents();
+  }
+
+  modalOpen = () => {
+    this.modalOverlayElement.classList.add(this.stateClasses.modalOverlayIsActive);
+    this.modalOverlayInnerElement.classList.add(this.stateClasses.modalOverlayInnerIsActive);
+    this.bodyElement.classList.add(this.stateClasses.modalOpen);
+  }
+
+  modalClose = () => {
+    this.modalOverlayElement.classList.remove(this.stateClasses.modalOverlayIsActive);
+    this.modalOverlayInnerElement.classList.remove(this.stateClasses.modalOverlayInnerIsActive);
+    this.bodyElement.classList.remove(this.stateClasses.modalOpen);
+  }
+
+  onModalLinkClick = () => {
+    this.modalClose();
+  };
+
+  bindEvents() {
+    this.burgerButtonElement.addEventListener("click", this.modalOpen);
+    this.modalCloseButtonElement.addEventListener("click", this.modalClose);
+
+    this.modalLinks.forEach((modalLink) => {
+      modalLink.addEventListener("click", this.onModalLinkClick);
+    });
+  }
+}
+
+export default ModalOverlay;
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Tabs from "./Tabs.js";
 import VideosCollection from "./Videos.js";
+import ModalOverlay from "./ModalOverlay.js";
 
 new Tabs();
 new VideosCollection();
+new ModalOverlay();
